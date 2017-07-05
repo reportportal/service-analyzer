99f583c1b752f4763e1663b95edcbb51b0b56356
Ivan Sharamet <isharamet@gmail.com>
2017-07-05 14:39:52 +0300

    First attempt in writing tests for ES client code

---
+++ esclient_test.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const TwoIndicesResponse = `
+	[
+		{
+			"health": "yellow",
+			"status": "open",
+			"index": "idx0",
+			"uuid": "sGD-VQy5StS1jIUbuo3R7A",
+			"pri": "1",
+			"rep": "1",
+			"docs.count": "353400",
+			"docs.deleted": "0",
+			"store.size": "37.9mb",
+			"pri.store.size": "37.9mb"
+		},
+		{
+			"health": "yellow",
+			"status": "open",
+			"index": "idx1",
+			"uuid": "DoA20IojS72IdaFSN8CX9Q",
+			"pri": "1",
+			"rep": "1",
+			"docs.count": "38771",
+			"docs.deleted": "0",
+			"store.size": "11.2mb",
+			"pri.store.size": "11.2mb"
+		}
+	]`
+
+const IndexCreatedResponse = `
+	{
+	"acknowledged" : true,
+	"shards_acknowledged" : true
+	}`
+
+const IndexAlreadyExistsResponse = `
+	{
+	"error" : {
+		"root_cause" : [
+		{
+			"type" : "index_already_exists_exception",
+			"reason" : "index [idx1/DoA20IojS72IdaFSN8CX9Q] already exists",
+			"index_uuid" : "DoA20IojS72IdaFSN8CX9Q",
+			"index" : "idx1"
+		}
+		],
+		"type" : "index_already_exists_exception",
+		"reason" : "index [idx1/DoA20IojS72IdaFSN8CX9Q] already exists",
+		"index_uuid" : "DoA20IojS72IdaFSN8CX9Q",
+		"index" : "idx1"
+	},
+	"status" : 400
+	}`
+
+func TestListIndices(t *testing.T) {
+	tests := []struct {
+		params        map[string]interface{}
+		expectedCount int
+		expectErr     bool
+	}{
+		{
+			params: map[string]interface{}{
+				"statusCode": http.StatusOK,
+				"response":   "[]",
+			},
+			expectedCount: 0,
+			expectErr:     false,
+		},
+		{
+			params: map[string]interface{}{
+				"statusCode": http.StatusOK,
+				"response":   TwoIndicesResponse,
+			},
+			expectedCount: 2,
+			expectErr:     false,
+		},
+		{
+			params: map[string]interface{}{
+				"statusCode": http.StatusInternalServerError,
+				"response":   "{}",
+			},
+			expectedCount: 0,
+			expectErr:     true,
+		},
+	}
+
+	for _, test := range tests {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			expectedMethod := "GET"
+			actualMethod := r.Method
+			assert.Equal(t, expectedMethod, actualMethod)
+			expectedURI := "/_cat/indices?format=json"
+			actualURI := r.URL.RequestURI()
+			assert.Equal(t, expectedURI, actualURI)
+			w.WriteHeader(test.params["statusCode"].(int))
+			w.Write([]byte(test.params["response"].(string)))
+		}))
+		defer ts.Close()
+
+		c := NewClient(ts.URL)
+
+		indices, err := c.ListIndices()
+		if test.expectErr {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+			idxs := *indices
+			assert.Equal(t, test.expectedCount, len(idxs))
+			for j, idx := range idxs {
+				assert.Equal(t, fmt.Sprintf("idx%d", j), idx.Index)
+			}
+		}
+	}
+}
+
+func TestCreateIndex(t *testing.T) {
+	tests := []struct {
+		params    map[string]interface{}
+		expectErr bool
+	}{
+		{
+			params: map[string]interface{}{
+				"indexName":  "idx0",
+				"statusCode": http.StatusOK,
+				"response":   IndexCreatedResponse,
+			},
+			expectErr: false,
+		},
+		{
+			params: map[string]interface{}{
+				"indexName":  "idx1",
+				"statusCode": http.StatusBadRequest,
+				"response":   IndexAlreadyExistsResponse,
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		indexName := test.params["indexName"].(string)
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			expectedMethod := "PUT"
+			actualMethod := r.Method
+			assert.Equal(t, expectedMethod, actualMethod)
+			expectedURI := "/" + indexName
+			actualURI := r.URL.RequestURI()
+			assert.Equal(t, expectedURI, actualURI)
+			w.WriteHeader(test.params["statusCode"].(int))
+			w.Write([]byte(test.params["response"].(string)))
+		}))
+		defer ts.Close()
+
+		c := NewClient(ts.URL)
+
+		rs, err := c.CreateIndex(indexName)
+		if test.expectErr {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+			assert.True(t, rs.Acknowledged)
+		}
+	}
+}

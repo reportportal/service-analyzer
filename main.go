b617848e9af7ffe3e6c283060d5d87d46bdeaba6
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-10-27 15:34:26 +0300

    fix default service tags

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 
 	defCfg := conf.EmptyConfig()
 	defCfg.Consul.Address = "registry:8500"
-	defCfg.Consul.Tags = []string{"urlprefix-/analyzer opts strip=/analyzer,analyzer=ML,analyzer_index=true,analyzer_priority=0"}
+	defCfg.Consul.Tags = []string{"urlprefix-/analyzer opts strip=/analyzer","analyzer=ML","analyzer_index=true","analyzer_priority=0"}
 	cfg := struct {
 		*conf.RpConfig
 		ESHosts []string `env:"ES_HOSTS" envDefault:"http://elasticsearch:9200"`

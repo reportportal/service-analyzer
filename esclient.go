c937c217c03d92acd679a8a09a6892a51d79fe4d
Pavel_Bortnik <pavel_bortnik@epam.com>
2017-10-31 18:27:12 +0300

    add term frequency

--- esclient.go
+++ esclient.go
@@ -374,6 +374,7 @@ func buildQuery(launchName, uniqueID, logMessage string) interface{} {
 							"fields":               []string{"message"},
 							"like":                 logMessage,
 							"min_doc_freq":         1,
+							"min_term_freq": 		1,
 							"minimum_should_match": "90%",
 						},
 					},
